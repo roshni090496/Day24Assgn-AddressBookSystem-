8a86f17c3b0335226609bb1f52bc20e30579aec2
roshni090496 <rs09041001@gmail.com>
2022-06-15 01:43:34 -0700

    Added : Address Book System Code

---
+++ src/com/company/InterfaceOne.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.List;
+
+    public interface InterfaceOne {
+
+        void createContact();
+        void addContact();
+        void editContact();
+        void deleteContact();
+        void display();
+        void displayBook();
+        void addToBook();
+        void operation();
+        void searchPerson();
+        void addContactByCity();
+        void viewByCity();
+        void addContactByState();
+        void viewByState();
+        void sortAddressBook();
+        void sortByCity();
+        void sortByState();
+        void writeToAddressBookFile();
+        public List<String> readDataFromFile();
+    }
+
